6e29dc4041e5928def5999af4ccacafcbd67fa4c
Jevee <madianbtorres@gmail.com>
2024-11-19 06:38:04 -0500

    added code to api

---
+++ routes/api/tag-routes.js
@@ -0,0 +1,76 @@
+const router = require('express').Router();
+const { where } = require('sequelize');
+const { Tag, Product, ProductTag } = require('../../models');
+
+router.get('/', (req, res) => {
+  Tag.findAll({
+    include: [
+      {
+        model: Product,
+        through: ProductTag,
+      },
+    ],  
+  })
+  .then((tags) => res.json(tags))
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
+
+router.get('/:id', (req, res) => {
+  Tag.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [
+      {
+        model: Product,
+        through: ProductTag,
+      },
+    ],
+  })
+    .then((tag) => res.json(tag))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+});
+
+router.post('/', (req, res) => {
+  Tag.create(req.body)
+    .then((tag) => res.json(tag))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+  
+});
+
+router.put('/:id', (req, res) => {
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((tag) => res.json(tag))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+});
+
+router.delete('/:id', (req, res) => {
+  Tag.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((tag) => res.json(tag))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+});
+
+module.exports = router;
